b0e24632b7b9d2c0ed5d503e2db1eda64318487e
DylanHendrickx <dylan.hendrickx@student.ucll.be>
2023-11-24 23:14:10 +0100

    index change

--- javascript/login.js
+++ javascript/login.js
@@ -5,7 +5,7 @@ function login() {
     // Simple check for demo purposes (replace with more secure methods)
     if (username === 'AlDy' && password === 'DyAl') {
         localStorage.setItem('authenticated', 'true');
-        window.location.href = 'index.html';
+        window.location.href = '../index.html';
     } else {
         alert('Invalid credentials. Please try again.');
     }
